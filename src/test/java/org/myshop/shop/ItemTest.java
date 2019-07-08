630a9210ee1c5eadf6cb348405e4e0c4c6afb0c7
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-08 21:58:50 +0300

    Introduce Item model object

---
+++ src/test/java/org/myshop/shop/ItemTest.java
@@ -0,0 +1,18 @@
+package org.myshop.shop;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.myshop.shop.model.Item;
+
+public class ItemTest {
+
+	@Test
+	public void testItemFieldsNotNullOnNewItem() {
+		Item item = new Item();
+		
+		assertNotNull(item.getId());
+		assertNotNull(item.getName());
+		assertNotNull(item.getDescription());
+	}
+}
