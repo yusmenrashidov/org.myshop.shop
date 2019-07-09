3dc282003d9e62806ddd85eeffe28e047f48aa71
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-09 17:58:57 +0300

    introducing PurchaseOrder object and PurchaseOrderTest object

---
+++ src/test/java/org/myshop/shop/PurchaseOrderTest.java
@@ -0,0 +1,22 @@
+package org.myshop.shop;
+
+import org.junit.Test;
+import org.myshop.shop.model.PurchaseOrder;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+
+public class PurchaseOrderTest {
+
+	
+	@Test
+	public void prurchaseOrderFieldsNotNull() {
+		PurchaseOrder purchaseOrder = new PurchaseOrder();
+		
+		assertNotNull(purchaseOrder.getId());
+		assertNotNull(purchaseOrder.getNumber());
+		assertNull(purchaseOrder.getCreated());
+	
+	}
+	
+}
