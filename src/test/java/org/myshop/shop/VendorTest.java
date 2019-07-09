409eecad9c3ebd4e0d9ee94ae8f24cafd10df7d3
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-09 16:00:26 +0300

    introducing Vendor object and VendorTest object

---
+++ src/test/java/org/myshop/shop/VendorTest.java
@@ -0,0 +1,17 @@
+package org.myshop.shop;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.myshop.shop.model.Vendor;
+
+public class VendorTest {
+	@Test
+	public void testItemFieldsNotNullOnNewItem() {
+		Vendor vendor = new Vendor();
+		
+		assertNotNull(vendor.getId());
+		assertNotNull(vendor.getName());
+	
+	}
+
+}
