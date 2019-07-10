bc2dce71b61c8519d64e1a0185b72a1c9f2b8c9c
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-10 11:46:56 +0300

    Removed unnecessary field from SalesOrder

--- src/test/java/org/myshop/shop/SalesOrderTest.java
+++ src/test/java/org/myshop/shop/SalesOrderTest.java
@@ -2,7 +2,6 @@ package org.myshop.shop;
 
 import org.myshop.shop.model.SalesOrder;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -16,7 +15,6 @@ public class SalesOrderTest {
 		assertNotNull(salesOrder.getId());
 		assertNull(salesOrder.getCreated());
 		assertNull(salesOrder.getCustomer());
-		assertEquals(0d, salesOrder.getLineNumber(), 0);
 	
 	}
 }
