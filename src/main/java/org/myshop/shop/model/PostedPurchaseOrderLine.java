0e9a100e52756bc1921a259c492b19e68bf76d9b
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-10 13:40:39 +0300

    Introducing posted versions of Sales and Purchase order classes

---
+++ src/main/java/org/myshop/shop/model/PostedPurchaseOrderLine.java
@@ -0,0 +1,49 @@
+package org.myshop.shop.model;
+
+public class PostedPurchaseOrderLine {
+	
+	private String id="";
+	private int lineNumber;
+	private Item item;
+	private int quantity;
+	private float price;
+	private int ammount;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getLineNumber() {
+		return lineNumber;
+	}
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getAmmount() {
+		return ammount;
+	}
+	public void setAmmount(int ammount) {
+		this.ammount = ammount;
+	}
+	
+}
---
+++ src/test/java/org/myshop/shop/PostedPurchaseOrderTest.java
@@ -0,0 +1,20 @@
+package org.myshop.shop;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import org.myshop.shop.model.PostedPurchaseOrder;
+
+public class PostedPurchaseOrderTest {
+
+	@Test
+	public void PostedPurchaseOrderFieldsNotNull() {
+		PostedPurchaseOrder purchaseOrder = new PostedPurchaseOrder();
+		
+		assertNotNull(purchaseOrder.getId());
+		assertNotNull(purchaseOrder.getNumber());
+		assertNull(purchaseOrder.getCreated());
+	
+	}
+	
+}
