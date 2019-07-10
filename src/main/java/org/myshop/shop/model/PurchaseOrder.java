91233c6d61d8180c754c4140b9cf51701d4e3625
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-10 10:37:56 +0300

    editted number field and removed empty lines int test

---
+++ src/main/java/org/myshop/shop/model/PurchaseOrder.java
@@ -0,0 +1,30 @@
+package org.myshop.shop.model;
+
+import java.util.Date;
+
+public class PurchaseOrder {
+
+	private String id=" ";
+	private String number=" ";
+	private Date created;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+}
