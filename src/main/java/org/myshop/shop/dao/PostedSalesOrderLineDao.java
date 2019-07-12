430f979ac24f926f1a60dbb9633a603f07675965
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-12 10:05:24 +0300

    Introducing last dao objects

---
+++ src/main/java/org/myshop/shop/dao/PostedSalesOrderLineDao.java
@@ -0,0 +1,14 @@
+package org.myshop.shop.dao;
+
+import java.util.List;
+import org.myshop.shop.model.Item;
+import org.myshop.shop.model.PostedSalesOrderLine;
+
+
+public interface PostedSalesOrderLineDao {
+	
+	public void addPostedSalesOrderLine(PostedSalesOrderLine line);
+	public List<PostedSalesOrderLine> getSalesOrderLines();
+	public List<PostedSalesOrderLine> getSalesOrderLinesByItem(Item imtem);
+	public PostedSalesOrderLine getSalesOrderLine(String id);
+}
---
+++ src/main/java/org/myshop/shop/dao/SalesOrderLineDao.java
@@ -0,0 +1,13 @@
+package org.myshop.shop.dao;
+
+import java.util.List;
+import org.myshop.shop.model.Item;
+import org.myshop.shop.model.SalesOrderLine;
+
+public interface SalesOrderLineDao {
+
+	public void addSalesOrderLine(SalesOrderLine line);
+	public List<SalesOrderLine> getSalesOrderLines();
+	public List<SalesOrderLine> getSalesOrderLinesByItem(Item imtem);
+	public SalesOrderLine getSalesOrderLine(String id);
+}
