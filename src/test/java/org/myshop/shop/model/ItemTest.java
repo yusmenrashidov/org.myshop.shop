185ab0adcc8d41561a2591468f2dcf798a3c2f70
rrashidov <rrashidov@gmail.com>
2019-07-12 11:42:19 +0300

    Enhance Item unit test (#46)

--- src/test/java/org/myshop/shop/model/ItemTest.java
+++ src/test/java/org/myshop/shop/model/ItemTest.java
@@ -9,15 +9,23 @@ import org.myshop.shop.model.Item;
 
 public class ItemTest {
 
-	@Test
-	public void testItemFieldsNotNullOnNewItem() {
-		Item item = new Item();
-		
-		assertNotNull(item.getId());
-		assertNotNull(item.getName());
-		assertNotNull(item.getDescription());
-		assertNull(item.getItemCategory());
-		assertEquals(0f, item.getSalesPrice(), 0);
-		assertEquals(0f, item.getSalesPrice(), 0);
-	}
+    @Test
+    public void testItemFieldsNotNullOnNewItem() {
+        Item item = new Item();
+
+        assertNotNull("Id should not be empty on new Item", item.getId());
+        assertEquals("Id should be empty on new Item", "", item.getId());
+
+        assertNotNull("Name should not be null on new Item", item.getName());
+        assertEquals("Name should be empty on new Item", "", item.getName());
+
+        assertNotNull("Description should not be null on new Item", item.getDescription());
+        assertEquals("Description should be empty on new Item", "", item.getDescription());
+
+        assertNull("ItemCategory should be null on new Item", item.getItemCategory());
+        assertNull("ProductGroup should be null on new Item", item.getProductGroup());
+
+        assertEquals("Sales price should be zero on new Item", 0f, item.getSalesPrice(), 0);
+        assertEquals("Purchase price should be zero on new Item", 0f, item.getSalesPrice(), 0);
+    }
 }
