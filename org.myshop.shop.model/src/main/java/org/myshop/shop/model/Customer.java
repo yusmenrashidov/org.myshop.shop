84fbc39b385ccdf087f508f3e9f8c285ebce55cd
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-17 11:51:18 +0300

    Removed unneeded constructors in Customer and Vendor classes

--- org.myshop.shop.model/src/main/java/org/myshop/shop/model/Customer.java
+++ org.myshop.shop.model/src/main/java/org/myshop/shop/model/Customer.java
@@ -2,7 +2,5 @@ package org.myshop.shop.model;
 
 public class Customer extends Partner {
 
-  public Customer() {
-	  super();
-  }
+
 }
--- org.myshop.shop.model/src/main/java/org/myshop/shop/model/Vendor.java
+++ org.myshop.shop.model/src/main/java/org/myshop/shop/model/Vendor.java
@@ -2,8 +2,6 @@ package org.myshop.shop.model;
 
 public class Vendor extends Partner{
 
-	public Vendor() {
-		super();
-	}
+	
 	
 }
