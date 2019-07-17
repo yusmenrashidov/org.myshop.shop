95a114d6388a3c0b222f920a7ca48ffbaff57208
Rashidov Rashidov <rashid.rashidov@sap.com>
2019-07-17 11:54:01 +0300

    Rework ItemDao in more harmonic way

--- org.myshop.shop.dao/src/main/java/org/myshop/shop/dao/ItemDao.java
+++ org.myshop.shop.dao/src/main/java/org/myshop/shop/dao/ItemDao.java
@@ -1,19 +1,18 @@
 package org.myshop.shop.dao;
 
 import java.util.List;
+
 import org.myshop.shop.model.Item;
-import org.myshop.shop.model.ItemCategory;
-import org.myshop.shop.model.ProductGroup;
 
 public interface ItemDao {
-	
-	public void addItem(Item item);
-	public List<Item> getItems();
-	public List<Item> getItemsByProductGroup(ProductGroup productGroup);
-	public List<Item> getItemsByItemCategory(ItemCategory category);
-	public Item getItem(String id);
-	public void editItem(Item item);
-	public void deleteItem(String id);
-	
 
+    public void create(Item item);
+
+    public List<Item> read();
+    
+    public Item get(String id);
+
+    public Item update(Item item);
+
+    public void delete(Item item);
 }
