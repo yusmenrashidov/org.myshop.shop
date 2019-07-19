69fa81c2a9e68a13dbf36b5c5705e00765217f9b
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-19 12:09:14 +0300

    Fixed ItemDao test

--- org.myshop.shop.dao.jdbc/src/test/java/org/myshop/shop/dao/jdbc/JdbcItemDaoTest.java
+++ org.myshop.shop.dao.jdbc/src/test/java/org/myshop/shop/dao/jdbc/JdbcItemDaoTest.java
@@ -2,18 +2,20 @@ package org.myshop.shop.dao.jdbc;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.anyString;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.*;
 import org.myshop.shop.model.Item;
 import org.myshop.shop.model.ItemCategory;
 import org.myshop.shop.model.ProductGroup;
@@ -38,8 +40,15 @@ public class JdbcItemDaoTest {
     private ItemCategory itemCategory;
     
     @Mock
+    private ResultSet rsMock;
+    
+    @Mock
     private PreparedStatement preparedStatementMock;
+    
+    @Mock
+    private List<Item> listMock;
 
+    @Mock
     private JdbcItemDao itemDao;
 
     @Before
@@ -47,6 +56,7 @@ public class JdbcItemDaoTest {
         MockitoAnnotations.initMocks(this);
         
         when(sqlConnectionMock.prepareStatement(anyString())).thenReturn(preparedStatementMock);
+        when(preparedStatementMock.executeQuery()).thenReturn(rsMock);
         
         when(itemMock.getId()).thenReturn(TEST_ITEM_ID);
         when(itemMock.getName()).thenReturn(TEST_ITEM_NAME);
@@ -55,10 +65,7 @@ public class JdbcItemDaoTest {
         when(itemMock.getItemCategory()).thenReturn(itemCategory);
         when(itemMock.getPurchasePrice()).thenReturn(0.0f);
         when(itemMock.getSalesPrice()).thenReturn(0.0f);
-       
-        
-        itemDao = new JdbcItemDao(sqlConnectionMock);
-        
+        itemDao = new JdbcItemDao(sqlConnectionMock);        
     }
     
     @Test
@@ -76,14 +83,49 @@ public class JdbcItemDaoTest {
         verify(preparedStatementMock).executeUpdate();
     }
     
+    @Test
     public void testRead() throws SQLException{
-    		
-    	verify(itemDao.read()).isEmpty();
+    	itemDao.read();
+    	
+    	verify(sqlConnectionMock).prepareStatement(anyString());
     	verify(preparedStatementMock).executeQuery();
+    	verify(rsMock).next();
+    	
+    	assertNotNull(itemDao.read());
     }
     
     @Test
     public void testGet() throws SQLException{
+    	itemDao.get(TEST_ITEM_ID);
+    	
+    	verify(sqlConnectionMock).prepareStatement(anyString());
+    	verify(preparedStatementMock).setString(1, TEST_ITEM_ID);
+    	verify(preparedStatementMock).executeQuery();
+     }
+    
+    @Test
+    public void testUpdate() throws SQLException{
+    	itemDao.update(itemMock);
+    	
+    	verify(sqlConnectionMock).prepareStatement(anyString());
+    	verify(preparedStatementMock).setString(1, itemMock.getId());
+    	verify(preparedStatementMock).setString(2, itemMock.getName());
+    	verify(preparedStatementMock).setString(3, itemMock.getDescription());
+    	verify(preparedStatementMock).setString(4, itemMock.getProductGroup().getId());
+    	verify(preparedStatementMock).setString(5, itemMock.getItemCategory().getId());
+    	verify(preparedStatementMock).setFloat(6, itemMock.getPurchasePrice());
+    	verify(preparedStatementMock).setFloat(7, itemMock.getSalesPrice());
+    	verify(preparedStatementMock).setString(8, itemMock.getId());
+    	verify(preparedStatementMock).executeUpdate();
+    }
+    
+    @Test
+    public void testDelete() throws SQLException{
+    	itemDao.delete(itemMock);
+    	
+    	verify(sqlConnectionMock).prepareStatement(anyString());
+    	verify(preparedStatementMock).setString(1, itemMock.getId());
+    	verify(preparedStatementMock).executeUpdate();
     	
     }
     
