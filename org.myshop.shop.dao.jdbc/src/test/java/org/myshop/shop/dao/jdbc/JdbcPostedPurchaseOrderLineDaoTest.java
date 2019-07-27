7e94ad33a73d19d1aba7732bec604aaf98313733
Yusmen Rashidov <40317174+yusmenrashidov@users.noreply.github.com>
2019-07-27 11:27:40 +0300

    Fixed formatting

--- org.myshop.shop.dao.jdbc/src/test/java/org/myshop/shop/dao/jdbc/JdbcPostedPurchaseOrderLineDaoTest.java
+++ org.myshop.shop.dao.jdbc/src/test/java/org/myshop/shop/dao/jdbc/JdbcPostedPurchaseOrderLineDaoTest.java
@@ -58,10 +58,10 @@ public class JdbcPostedPurchaseOrderLineDaoTest {
 	@Mock
 	private ResultSet itemResultSetMock;
 	
-    @Mock
-    private ResultSet productGroupResultSetMock;
+    	@Mock
+    	private ResultSet productGroupResultSetMock;
 	    
-    @Mock
+    	@Mock
 	private ResultSet itemCategoryResulSetMock;
 	
 	@Mock
@@ -89,18 +89,18 @@ public class JdbcPostedPurchaseOrderLineDaoTest {
 	private static final int TEST_POSTED_PURCHASE_ORDER_LINE_AMMOUNT = (int) 123d;
 	
 	private static final String TEST_ITEM_ID = "test_item_id";
-    private static final String TEST_ITEM_NAME = "test_item_name";
-    private static final String TEST_ITEM_DESCRIPTION = "test_item_description";
+    	private static final String TEST_ITEM_NAME = "test_item_name";
+    	private static final String TEST_ITEM_DESCRIPTION = "test_item_description";
    
-    private static final String TEST_PRODUCT_GROUP_ID = "test_product_group_id";
-    private static final String TEST_PRODUCT_GROUP_DESCRIPTION = "test_product_group_description";
+    	private static final String TEST_PRODUCT_GROUP_ID = "test_product_group_id";
+    	private static final String TEST_PRODUCT_GROUP_DESCRIPTION = "test_product_group_description";
   
-    private static final String TEST_ITEM_CATEGORY_ID = "test_item_category_id";
-    private static final String TEST_ITEM_CATEGORY_NAME = "test_item_category_name";
-    private static final String TEST_ITEM_CATEGORY_DESCRIPTION = "test_item_category_description";
+    	private static final String TEST_ITEM_CATEGORY_ID = "test_item_category_id";
+    	private static final String TEST_ITEM_CATEGORY_NAME = "test_item_category_name";
+    	private static final String TEST_ITEM_CATEGORY_DESCRIPTION = "test_item_category_description";
    
-    private static final float TEST_PURCHASE_PRICE = 123.123f;
-    private static final float TEST_SALES_PRICE = 234.234f;
+    	private static final float TEST_PURCHASE_PRICE = 123.123f;
+    	private static final float TEST_SALES_PRICE = 234.234f;
 		
     @Before
     public void setup() throws SQLException {
