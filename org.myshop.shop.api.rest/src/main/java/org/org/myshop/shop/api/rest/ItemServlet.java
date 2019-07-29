d5ec73899d6bc0373d3fa2f760a0ae15518d5d9c
Rashidov Rashidov <rashid.rashidov@sap.com>
2019-07-29 21:01:57 +0300

    ItemServlet sample unit test

--- org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemServlet.java
+++ org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemServlet.java
@@ -1,54 +1,67 @@
 package org.org.myshop.shop.api.rest;
 
-import javax.servlet.http.HttpServlet;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.sql.Connection;
-
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.myshop.shop.dao.jdbc.JdbcItemDao;
+import org.myshop.shop.dao.ItemDao;
 import org.myshop.shop.model.Item;
-
-import com.google.gson.Gson;
+import org.org.myshop.shop.api.rest.servlet.util.IItemDeserializer;
+import org.org.myshop.shop.api.rest.servlet.util.IRequestBodyReader;
 
 
 @WebServlet(name = "itemServlet", urlPatterns = {"/api/v1/model/item"})
 public class ItemServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private Connection sqlConnection;
+
+	private IRequestBodyReader requestBodyReader;
+	
+	private IItemDeserializer itemDeserializer;
+	
+	private ItemDao itemDao;
 	
 	@Override
 	public void doPut(HttpServletRequest request, HttpServletResponse response) {
-		
-			JdbcItemDao itemDao = new JdbcItemDao(sqlConnection);
-			
-					try {
-						itemDao.create(requestToObject(request));
-					} catch (IOException e) {
-					
-						e.printStackTrace();
-					}
-		
-		
+	    
+	    String requestBody = requestBodyReader.readBody(request);
+	    
+	    Item item = itemDeserializer.deserialize(requestBody);
+	    
+	    itemDao.create(item);
 	}
 	
-	public Item requestToObject(HttpServletRequest request) throws IOException {
-		
-		BufferedReader reader = request.getReader();
-		
-		Item item = new Gson().fromJson(reader, Item.class);
-		
-		return item;
-	}
-	
+    public IRequestBodyReader getRequestBodyReader() {
+        if (requestBodyReader == null) {
+            requestBodyReader = new RequestBodyReader();
+        }
+        return requestBodyReader;
+    }
 
+    public IItemDeserializer getItemDeserializer() {
+        if (itemDeserializer == null) {
+            itemDeserializer = new ItemDeserializer();
+        }
+        return itemDeserializer;
+    }
+
+    public ItemDao getItemDao() {
+        if (itemDao == null) {
+            //TODO Init itemDao
+        }
+        return itemDao;
+    }
+
+    public void setRequestBodyReader(IRequestBodyReader requestBodyReader) {
+        this.requestBodyReader = requestBodyReader;
+    }
+
+    public void setItemDeserializer(IItemDeserializer itemDeserializer) {
+        this.itemDeserializer = itemDeserializer;
+    }
+
+    public void setItemDao(ItemDao itemDao) {
+        this.itemDao = itemDao;
+    }
 }
---
+++ org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/RequestBodyReader.java
@@ -0,0 +1,14 @@
+package org.org.myshop.shop.api.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.org.myshop.shop.api.rest.servlet.util.IRequestBodyReader;
+
+public class RequestBodyReader implements IRequestBodyReader {
+
+    public String readBody(HttpServletRequest request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
