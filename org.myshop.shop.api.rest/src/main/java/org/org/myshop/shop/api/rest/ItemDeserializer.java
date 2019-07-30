971febd6d15a1797e38daffba8f8ce8ed5f58006
yusmenrashidov <jusmen.rashidov@gmail.com>
2019-07-30 10:39:45 +0300

    Implemented deserializer and request body reader

--- org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemDeserializer.java
+++ org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemDeserializer.java
@@ -1,13 +1,27 @@
 package org.org.myshop.shop.api.rest;
 
+import java.io.IOException;
+
 import org.myshop.shop.model.Item;
 import org.org.myshop.shop.api.rest.servlet.util.IItemDeserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class ItemDeserializer implements IItemDeserializer {
 
     public Item deserialize(String itemJsonString) {
-        // TODO Auto-generated method stub
-        return null;
+       
+    	Item item = null;
+    	ObjectMapper mapper = new ObjectMapper();
+    	
+    	try {
+			item = mapper.readValue(itemJsonString, Item.class);
+		} catch (IOException e) {
+			return null;
+		}
+    	
+        return item;
     }
 
 }
--- org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/RequestBodyReader.java
+++ org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/RequestBodyReader.java
@@ -1,5 +1,8 @@
 package org.org.myshop.shop.api.rest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.org.myshop.shop.api.rest.servlet.util.IRequestBodyReader;
@@ -7,8 +10,22 @@ import org.org.myshop.shop.api.rest.servlet.util.IRequestBodyReader;
 public class RequestBodyReader implements IRequestBodyReader {
 
     public String readBody(HttpServletRequest request) {
-        // TODO Auto-generated method stub
-        return null;
+        
+    	StringBuilder buffer = new StringBuilder();
+    	BufferedReader reader;
+    	String requestLine;
+    	
+    	try {
+			reader = request.getReader();
+		
+			while((requestLine = reader.readLine()) != null)
+				buffer.append(requestLine);
+			
+    	} catch (IOException e) {
+			return null;
+		}
+    	
+        return buffer.toString();
     }
 
 }
