6f136df97557f2388953d7a5de5f95016c1d8b18
Rashidov Rashidov <rashid.rashidov@sap.com>
2019-08-14 10:06:13 +0300

    Remove unused imports in ItemServlet

--- org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemServlet.java
+++ org.myshop.shop.api.rest/src/main/java/org/org/myshop/shop/api/rest/ItemServlet.java
@@ -2,8 +2,6 @@ package org.org.myshop.shop.api.rest;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
