92c600b4bf3764f73f669887b56fc875a9a827f3
Rashidov Rashidov <rashid.rashidov@sap.com>
2019-08-14 10:58:00 +0300

    Introduce generic serializer

---
+++ org.myshop.shop.api.rest/src/test/java/org/org/myshop/shop/api/rest/servlet/util/MyClass.java
@@ -0,0 +1,22 @@
+package org.org.myshop.shop.api.rest.servlet.util;
+
+public class MyClass {
+
+    private String s;
+    private int i;
+    
+    public String getS() {
+        return s;
+    }
+    public void setS(String s) {
+        this.s = s;
+    }
+    public int getI() {
+        return i;
+    }
+    public void setI(int i) {
+        this.i = i;
+    }
+    
+    
+}
