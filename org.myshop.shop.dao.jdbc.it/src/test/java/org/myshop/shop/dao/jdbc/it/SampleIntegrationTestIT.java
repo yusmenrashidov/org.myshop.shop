ac0a01b71637ce1a2b07d83ebbe75d9b13d67a16
rrashidov <rrashidov@gmail.com>
2019-08-20 13:39:13 +0300

    Jdbc integration test (#119)
    * Initi jdbc it module
    
    * Sample DB integration test impl

---
+++ org.myshop.shop.dao.jdbc.it/src/test/java/org/myshop/shop/dao/jdbc/it/SampleIntegrationTestIT.java
@@ -0,0 +1,104 @@
+package org.myshop.shop.dao.jdbc.it;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class SampleIntegrationTestIT {
+
+    private static String dbURL = "jdbc:derby://localhost:1527/memory:myshop;create=true;user=myshop;password=myshop";
+    private static String tableName = "restaurants";
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    @Test
+    public void test() {
+        createConnection();
+        insertRestaurants(5, "LaVals", "Berkeley");
+        selectRestaurants();
+        shutdown();
+    }
+
+    private static void createConnection() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            // Get a connection
+            conn = DriverManager.getConnection(dbURL);
+        } catch (Exception except) {
+            except.printStackTrace();
+        }
+    }
+    
+    private static void insertRestaurants(int id, String restName, String cityName)
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            stmt.execute("insert into " + tableName + " values (" +
+                    id + ",'" + restName + "','" + cityName +"')");
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    } 
+    
+    private static void selectRestaurants()
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("select * from " + tableName);
+            ResultSetMetaData rsmd = results.getMetaData();
+            int numberCols = rsmd.getColumnCount();
+            for (int i=1; i<=numberCols; i++)
+            {
+                //print Column Names
+                System.out.print(rsmd.getColumnLabel(i)+"\t\t");  
+            }
+
+            System.out.println("\n-------------------------------------------------");
+
+            while(results.next())
+            {
+                int id = results.getInt(1);
+                String restName = results.getString(2);
+                String cityName = results.getString(3);
+                System.out.println(id + "\t\t" + restName + "\t\t" + cityName);
+            }
+            results.close();
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    }
+    
+    private static void shutdown()
+    {
+        try
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+            if (conn != null)
+            {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                conn.close();
+            }           
+        }
+        catch (SQLException sqlExcept)
+        {
+            
+        }
+
+    }
+}
---
+++ org.myshop.shop.dao.jdbc.it/src/test/resources/sql/test-data-derby.sql
@@ -0,0 +1,1 @@
+CREATE TABLE restaurants (id numeric, name varchar(100), city varchar(100));
