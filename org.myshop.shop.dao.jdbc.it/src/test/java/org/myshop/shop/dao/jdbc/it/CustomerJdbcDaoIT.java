796773cf3f83ff54af7afbbab614c1d02b0def92
rrashidov <rrashidov@gmail.com>
2019-08-27 15:11:49 +0300

    Improve Customer Jdbc Dao integration test (#131)

--- org.myshop.shop.dao.jdbc.it/src/test/java/org/myshop/shop/dao/jdbc/it/CustomerJdbcDaoIT.java
+++ org.myshop.shop.dao.jdbc.it/src/test/java/org/myshop/shop/dao/jdbc/it/CustomerJdbcDaoIT.java
@@ -17,6 +17,8 @@ public class CustomerJdbcDaoIT {
     private static final String TEST_CUSTOMER_ID = "test_customer_id";
     private static final String TEST_CUSTOMER_NAME = "test_customer_name";
     private static final String CHANGED_CUSTOMER_NAME = "changed_customer_name";
+    
+    private static final int TEST_CUSTOMER_COUNT = 123;
 
     private static String dbURL = "jdbc:derby://localhost:1527/memory:myshop;create=true;user=myshop;password=myshop";
 
@@ -34,12 +36,13 @@ public class CustomerJdbcDaoIT {
             except.printStackTrace();
         }
 
-        
         customerDao = new JdbcCustomerDao(connection);
+        
+        cleanup();
     }
     
     @Test
-    public void test() {
+    public void testCreate() {
         Customer customer = new Customer();
         customer.setId(TEST_CUSTOMER_ID);
         customer.setName(TEST_CUSTOMER_NAME);
@@ -54,17 +57,64 @@ public class CustomerJdbcDaoIT {
         assertEquals(TEST_CUSTOMER_ID, customerRetrievedFromDb.getId());
         assertEquals(TEST_CUSTOMER_NAME, customerRetrievedFromDb.getName());
         
-        customerRetrievedFromDb.setName(CHANGED_CUSTOMER_NAME);
+    }
+    
+    @Test
+    public void testRead() {
+        for (int i = 0; i < TEST_CUSTOMER_COUNT; i++) {
+            Customer customer = new Customer();
+            customer.setId(String.valueOf(i));
+            customer.setName("Customer " + i);
+            
+            customerDao.create(customer);
+        }
         
-        customerDao.update(customerRetrievedFromDb);
+        List<Customer> customerList = customerDao.read();
+        
+        assertEquals(TEST_CUSTOMER_COUNT, customerList.size());
+    }
+    
+    @Test
+    public void testUpdate() {
+        Customer customer = new Customer();
+        customer.setId(TEST_CUSTOMER_ID);
+        customer.setName(TEST_CUSTOMER_NAME);
+        
+        customerDao.create(customer);
+        
+        customer.setName(CHANGED_CUSTOMER_NAME);
+        
+        customerDao.update(customer);
+        
+        Customer customerReadFromDb = customerDao.get(TEST_CUSTOMER_ID);
+        
+        assertEquals(CHANGED_CUSTOMER_NAME, customerReadFromDb.getName());
+    }
+    
+    @Test
+    public void testDelete() {
+        Customer customer = new Customer();
+        customer.setId(TEST_CUSTOMER_ID);
+        customer.setName(TEST_CUSTOMER_NAME);
+        
+        customerDao.create(customer);
+        
+        List<Customer> customerList = customerDao.read();
+        
+        assertEquals(1, customerList.size());
+        
+        customerDao.delete(customer);
         
         customerList = customerDao.read();
-        customerRetrievedFromDb = customerList.get(0);
-        assertEquals(CHANGED_CUSTOMER_NAME, customerRetrievedFromDb.getName());
         
-        customerDao.delete(customerRetrievedFromDb);
-        
-        customerList = customerDao.read();
         assertEquals(0, customerList.size());
+    }
+    
+    private void cleanup() {
+        List<Customer> customerList = customerDao.read();
+        
+        for (Customer customer : customerList) {
+            customerDao.delete(customer);
+        }
     }
 }
