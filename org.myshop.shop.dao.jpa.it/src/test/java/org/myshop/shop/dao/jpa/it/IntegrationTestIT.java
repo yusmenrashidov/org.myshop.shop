4f1821513585324d48474814542b8ffaac3ff344
Yusmen Rashidov <40317174+yusmenrashidov@users.noreply.github.com>
2019-08-28 14:41:25 +0300

    Unexpected fails in JPA Customer IT (#132)
    * Unexpected fails in JPA Customer IT
    
    * Fixed test fail

--- org.myshop.shop.dao.jpa.it/src/test/java/org/myshop/shop/dao/jpa/it/IntegrationTestIT.java
+++ org.myshop.shop.dao.jpa.it/src/test/java/org/myshop/shop/dao/jpa/it/IntegrationTestIT.java
@@ -1,5 +1,9 @@
 package org.myshop.shop.dao.jpa.it;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -28,8 +32,74 @@ public class IntegrationTestIT {
 		customer.setId("test_id");
 		customer.setName("test_name");
 		
-		entityManager.persist(customer);
+		entityManager.persist(customer);	
 		
+		entityManager.getTransaction().commit();
 	}
+	
+	@Test
+	public void testGetData() throws Exception {
+		
+		this.setup();
+		this.testInsertSomeData();
+		
+	   CustomerEntity customer = entityManager.find(CustomerEntity.class, "test_id");
+		
+		assertNotNull(customer);
+		assertEquals(customer.getId(), "test_id");
+		assertEquals(customer.getName(), "test_name");
+	}
+	
+	@Test
+	public void testUpdate() throws Exception {
+		
+		this.setup();
+		entityManager.getTransaction().begin();
+		
+		CustomerEntity customer = new CustomerEntity();
+		customer.setId("update_id");
+		customer.setName("update_name");
+		
+		entityManager.persist(customer);	
+		
+		entityManager.getTransaction().commit();
+		
+	    customer = entityManager.find(CustomerEntity.class, "update_id");
+		
+		entityManager.getTransaction().begin();
+		customer.setName("new_name");
+		entityManager.getTransaction().commit();
+	
+	    customer = entityManager.find(CustomerEntity.class, "update_id");
+	    
+	    assertEquals(customer.getId(), "update_id");
+	    assertEquals(customer.getName(), "new_name");
+	
+	}
+	
+	@Test
+	public void testDelete() throws Exception {
+		
+		this.setup();
+		entityManager.getTransaction().begin();
+		
+		CustomerEntity customer = new CustomerEntity();
+		customer.setId("test_id_delete");
+		customer.setName("test_name_delete");
+		
+		entityManager.persist(customer);	
+		
+		entityManager.getTransaction().commit();
+		
+	    customer = entityManager.find(CustomerEntity.class, "test_id_delete");
+	
+	    entityManager.getTransaction().begin();
+		entityManager.remove(customer);
+		entityManager.getTransaction().commit();
+		
+		customer = entityManager.find(CustomerEntity.class, "test_id_delete");
+		assertNull(customer);
+	}
+	
 	
 }
