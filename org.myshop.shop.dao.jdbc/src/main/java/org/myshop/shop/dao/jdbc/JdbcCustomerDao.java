2e2aa4f8031795b5d97e41ce5a08341241d21956
rrashidov <rrashidov@gmail.com>
2019-09-12 15:34:02 +0300

    Fix formatting in JdbcCustomerDao (#151)

--- org.myshop.shop.dao.jdbc/src/main/java/org/myshop/shop/dao/jdbc/JdbcCustomerDao.java
+++ org.myshop.shop.dao.jdbc/src/main/java/org/myshop/shop/dao/jdbc/JdbcCustomerDao.java
@@ -11,113 +11,103 @@ import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class JdbcCustomerDao implements CustomerDao {
 
 	private Connection sqlConnection;
-	
+
 	protected static final String CREATE_QUERY = "INSERT INTO customer VALUES(?, ?)";
 	protected static final String READ_QUERY = "SELECT * FROM customer";
 	protected static final String GET_QUERY = "SELECT * FROM customer WHERE id = ?";
 	protected static final String UPDATE_QUERY = "UPDATE customer SET id = ?, name = ? WHERE id = ?";
 	protected static final String DELETE_QUERY = "DELETE FROM customer WHERE id = ?";
-	
+
 	public JdbcCustomerDao(Connection sqlConnection) {
 		this.sqlConnection = sqlConnection;
 	}
-	
+
 	public void create(Customer customer) {
-		
+
 		try {
 			PreparedStatement stmt = sqlConnection.prepareStatement(CREATE_QUERY);
 
 			stmt.setString(1, customer.getId());
 			stmt.setString(2, customer.getName());
-			
+
 			stmt.executeUpdate();
-			
 		} catch (SQLException e) {
-			
 			e.printStackTrace();
 		}
 	}
 
 	public List<Customer> read() {
-		
+
 		List<Customer> list = new ArrayList<Customer>();
-		
+
 		try {
 			PreparedStatement stmt = sqlConnection.prepareStatement(READ_QUERY);
 			ResultSet rs = stmt.executeQuery();
-		
-			while(rs.next()) {
-		        Customer customer = new Customer();
+
+			while (rs.next()) {
+				Customer customer = new Customer();
 				customer.setId(rs.getString("id"));
 				customer.setName(rs.getString("name"));
-				
+
 				list.add(customer);
 			}
-		
 		} catch (SQLException e) {
 			return null;
 		}
-		
+
 		return list;
 	}
 
 	public Customer get(String id) {
-		
+
 		Customer customer = new Customer();
-		
+
 		try {
 			PreparedStatement stmt = sqlConnection.prepareStatement(GET_QUERY);
-			
+
 			stmt.setString(1, id);
-			
+
 			ResultSet rs = stmt.executeQuery();
-			
-			if(rs.next()) {
-			customer.setId(rs.getString("id"));
-			customer.setName(rs.getString("name"));
+
+			if (rs.next()) {
+				customer.setId(rs.getString("id"));
+				customer.setName(rs.getString("name"));
 			}
-			
 		} catch (SQLException e) {
 			return null;
 		}
-		
+
 		return customer;
 	}
 
 	public Customer update(Customer customer) {
-		
-		
-		
+
 		try {
-		
+
 			PreparedStatement stmt = sqlConnection.prepareStatement(UPDATE_QUERY);
-			
+
 			stmt.setString(1, customer.getId());
-			stmt.setString(2,  customer.getName());
+			stmt.setString(2, customer.getName());
 			stmt.setString(3, customer.getId());
 			stmt.executeUpdate();
-		
 		} catch (SQLException e) {
 			return null;
 		}
-		
+
 		return customer;
 	}
 
 	public void delete(Customer customer) {
-		
+
 		try {
 			PreparedStatement stmt = sqlConnection.prepareStatement(DELETE_QUERY);
 			stmt.setString(1, customer.getId());
-			
-			stmt.executeUpdate();
-			
-		} catch (SQLException e) {
 
+			stmt.executeUpdate();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
